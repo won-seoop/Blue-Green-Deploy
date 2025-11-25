8877f737df7b19b94854a670291a632fd2d75187
halo <dongjin778@naver.com>
2025-11-25 15:14:27 +0900

    d

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// 드가자ㄴ
 app.get('/health', (req, res) => {
     const now = new Date();
     const hh = String(now.getHours()).padStart(2, '0');
